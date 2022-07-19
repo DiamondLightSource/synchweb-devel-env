7a2325a692016243c69e77fee8cede84c06b7231
Calum Byrom <calum.byrom@gmail.com>
2022-07-19 17:57:02 +0100

    Include install of additional set up tools

--- podman/setup_synchweb.bash
+++ podman/setup_synchweb.bash
@@ -4,7 +4,6 @@
 # Description   : Script to setup SynchWeb running in a podman container.  Note, the config.php file should be setup appropriately before running.
 # Args          : $1 - name of podman image to create and run - default: 'synchweb-dev', $2 - run initial container setup - default:'1' (run set up)
 ############################################################
-
 imageName=synchweb-dev
 if [ $1 ]
 then
@@ -28,9 +27,27 @@ if [ ! -d SynchWeb ]
 then
     echo Cloning SynchWeb locally
     git clone https://github.com/DiamondLightSource/SynchWeb.git
-    sudo dnf module switch-to nodejs:16 #module load node/16.15 # equivalent command in non-fedora linux distros
 
     echo Setting up web client
+    sudo apt-get -y update
+
+    echo Installing node....
+    sudo apt-get -y install curl
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    nvm install v16.15
+# if the above command doesn't work for your linux distro, try either of:
+#    sudo dnf module switch-to nodejs:16 
+#    module load node/16.15 
+
+    echo Installing webpack
+    sudo apt-get -y install webpack
+
+    echo Installing podman
+    sudo apt-get -y install podman
+
     cd SynchWeb/client
     npm install
     cd -
@@ -64,7 +81,6 @@ if [ -f index.php ]
 then
     unlink index.php
 fi
-
 npm run build:dev && export HASH=$(ls dist) && ln -sf dist/${HASH}/index.html index.php
 cd -
 
