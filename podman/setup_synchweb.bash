3796bae23b8d2c4de9868d0fd33bc1e87934bb19
cbyrom <calum.byrom@tessella.com>
2022-07-29 17:05:22 +0100

    Adjust install script to allow different linux distros install cmd to be used

--- podman/setup_synchweb.bash
+++ podman/setup_synchweb.bash
@@ -2,7 +2,9 @@
 ############################################################
 # Name          : setup_synchweb.bash
 # Description   : Script to setup SynchWeb running in a podman container.  Note, the config.php file should be setup appropriately before running.
-# Args          : $1 - name of podman image to create and run - default: 'synchweb-dev', $2 - run initial container setup - default:'1' (run set up)
+# Args          : $1 - name of podman image to create and run - default: 'synchweb-dev', 
+#               : $2 - run initial container setup - default:'1' (run set up)
+#               : $3 - install command to use - default:"sudo apt-get -y" - adjust for different linux distros
 ############################################################
 
 set -e # exit immediately if any command fails
@@ -13,20 +15,27 @@ imageName=synchweb-dev
 if [ $1 ]
 then
     imageName=$1
-    echo Creating podman image with name: $imageName
 fi
+echo Creating and running podman image with name: $imageName
 
 initialSetUp=1 # if '1' do an initial local set up - e.g. copying in the config files
 if [ $2 ]
 then
     initialSetUp=$2
-    if [ $initialSetUp -eq 1 ]
-    then
-        echo Will run initial set up
-    else
-        echo Will NOT run initial set up
-    fi
 fi
+if [ $initialSetUp -eq 1 ]
+then
+    echo Will run initial set up
+else
+    echo Will NOT run initial set up
+fi
+
+installCmd="sudo apt-get -y"
+if [ $3 ]
+then
+    installCmd=$3
+fi
+echo Install linux apps using: $installCmd
 
 if [ ! -d SynchWeb ]
 then
@@ -34,11 +43,11 @@ then
     git clone https://github.com/DiamondLightSource/SynchWeb.git
 
     echo Setting up web client locally
-    sudo apt-get -y update
+    $installCmd update
 
     # set up appropriate version of node to use (v18 will fail with current code)
     echo Installing node....
-    sudo apt-get -y install curl
+    $installCmd install curl
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -48,14 +57,14 @@ then
 #   sudo dnf module switch-to nodejs:16 
 #   module load node/16.15 
 
-    echo Installing webpack
-    sudo apt-get -y install webpack
-
     echo Installing podman
-    sudo apt-get -y install podman
+    $installCmd install podman
 
     cd SynchWeb/client
     npm install
+
+    echo Installing webpack
+    npm install webpack
     cd -
 fi
 
@@ -87,7 +96,7 @@ if [ -f index.php ]
 then
     unlink index.php
 fi
-npm run build:dev && export HASH=$(ls dist) && ln -sf dist/${HASH}/index.html index.php
+npm run build:dev && export HASH=$(ls -t dist | head -n1) && ln -sf dist/${HASH}/index.html index.php
 cd -
 
 echo Starting $imageName container
