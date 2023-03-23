f10bdeda094af27adac3b68545942fb0da47caeb
John Holt <John.Holt@diamond.ac.uk>
2023-03-23 14:14:58 +0000

    Hard code path to config files

--- podman/entrypoint.bash
+++ podman/entrypoint.bash
@@ -5,7 +5,7 @@ export COMPOSER_ALLOW_SUPERUSER=1 #This allows compose to run as root in this co
 cd /app/SynchWeb/api && /tmp/composer install && /tmp/composer update
 httpd -f /etc/httpd/conf/httpd.conf -k start
 if [[ "$1" == "-7" ]]; then
-    PHP_INI_SCAN_DIR=/etc/php.d php-fpm -e -y php-fpm.conf -F -O -c php.ini &
+    PHP_INI_SCAN_DIR=/etc/php.d php-fpm -e -y /app/SynchWeb/php-fpm.conf -F -O -c /app/SynchWeb/php.ini &
 else
     PHP_INI_SCAN_DIR=/opt/remi/php54/root/etc/php.d php-fpm -F -y /app/SynchWeb/php-fpm.conf -c /app/SynchWeb/php.ini &
 fi
